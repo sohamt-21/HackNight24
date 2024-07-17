c61e96d5e44408cdab75bad52422719991a6168a
sohamt-21 <sohamtak0403@gmail.com>
2024-07-18 00:57:34 +0530

    Conflict Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,11 @@ import "./index.css";
 import Home from "./Pages/Home/Home";
 import { ThemeProvider } from "./components/theme-provider";
 import { ReactLenis, useLenis } from "lenis/react";
-<<<<<<< HEAD
 import Dashboard from "./Pages/Dashboard";
-=======
 import Retrive from "./components/Retrive/Retrive"
 
 // import Retrive from "./components/Retrive/Retrive";
 import  Signup  from "./components/Portfoliosignup/Signup";
->>>>>>> b77fcec7520148d8b9f92645890102ba8673d3b9
 
 function App() {
   const lenis = useLenis(({ scroll }) => {});
@@ -19,20 +16,15 @@ function App() {
     <ReactLenis root>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         {/* <Home /> */}
-<<<<<<< HEAD
         <Dashboard/>
-=======
-        <Retrive/>
-        <Signup/>
->>>>>>> b77fcec7520148d8b9f92645890102ba8673d3b9
+        {/* <Retrive/>
+        <Signup/> */}
       </ThemeProvider>
       
     </ReactLenis>
-<<<<<<< HEAD
 
-=======
     
->>>>>>> b77fcec7520148d8b9f92645890102ba8673d3b9
+
   );
 }
 
